c55f6bb4f47cd49feac177dc8b5f507042698765
GitHub Action <action@github.com>
2023-02-01 02:59:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/lazmond3/intellijnotifybootready/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lazmond3/intellijnotifybootready/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lazmond3.intellijnotifybootready.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lazmond3.intellijnotifybootready.MyBundle
 
 class MyProjectService(project: Project) {
 
